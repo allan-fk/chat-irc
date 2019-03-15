033e4efbde449d5f54d36aacdafa32ffaf4856aa
allan <allankleinpro@gmail.com>
2019-03-15 16:18:12 +0100

    page not found

--- src/Router.js
+++ src/Router.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react'
 import {BrowserRouter, Route, Switch} from 'react-router-dom';
 import Connexion from './Components/Connexion'
+import error404 from './Components/error404'
 import App from './App'
 
 export default class Router extends Component {
@@ -10,6 +11,7 @@ export default class Router extends Component {
         <Switch>
             <Route exact path='/' component={Connexion} />
             <Route exact path='/pseudo/:pseudo' component={App} />
+            <Route component={error404} />
         </Switch>
     </BrowserRouter>
     )
