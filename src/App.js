5f30235cb421693ff0cad2d6aa0b5cfc9f1e685d
allan <allankleinpro@gmail.com>
2019-03-15 15:54:40 +0100

    test gitlab page

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import React, { Component } from 'react'
 import './App.css'
+import Connexion from './Components/Connexion'
 
 class App extends Component {
   render () {
     return (
-      <div className='box' />
+      <div className='box'>
+        <Connexion/>
+      </div>
     )
   }
 }
---
+++ src/Components/Connexion.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+
+export default class Connexion extends Component {
+  render() {
+    return (
+      <div className='connexionBox'>
+        <form className='connexion'>
+            <input placeholder='pseudo' type='text' required/>
+            <button type='submit'>Go!</button>
+        </form>
+        
+      </div>
+    )
+  }
+}
