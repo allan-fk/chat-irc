79193bc985821a9a261c0ffc35a8788c463c82d0
allan <allankleinpro@gmail.com>
2019-03-15 17:15:23 +0100

    try to fix gitlab pages routes 4

---
+++ src/Components/Toast.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class Toast extends Component {
+  render() {
+    return (
+      <div>
+        <h2>fefke,ckz</h2>
+      </div>
+    )
+  }
+}
--- src/Router.js
+++ src/Router.js
@@ -2,6 +2,8 @@ import React, { Component } from 'react'
 import {BrowserRouter, Route, Switch} from 'react-router-dom';
 import Connexion from './Components/Connexion'
 import error404 from './Components/error404'
+import Toast from './Components/Toast'
+
 import App from './App'
 
 export default class Router extends Component {
@@ -10,6 +12,7 @@ export default class Router extends Component {
       <BrowserRouter basename={process.env.PUBLIC_URL}>
         <Switch>
             <Route exact path='/' component={Connexion} />
+            <Route exact path='/toast' component={Toast} />
             <Route exact path='/pseudo/:pseudo' component={App} />
             <Route component={error404} />
           </Switch>
