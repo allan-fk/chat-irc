522916251c58a98f849b0359adda71c1c10e203c
allan <allankleinpro@gmail.com>
2019-03-20 19:07:25 +0100

    fix routes 5

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react'
 import './App.css'
-//import Connexion from './Components/Connexion'
+import Formulaire from './Components/Formulaire'
 
 class App extends Component {
   state = {
@@ -10,8 +10,12 @@ class App extends Component {
   render () {
     return (
       <div className='box'>
+        <div className='messages'>
+          Messages
+        </div>
         <h2>Bienvenue</h2>
         <h3>{this.state.url.substr(this.state.url.lastIndexOf('/') + 1)}</h3>
+        <Formulaire />
       </div>
     )
   }
