6d6d711d077062db86f58c6066ff6cf7abd849d8
allan <allankleinpro@gmail.com>
2019-03-20 17:53:05 +0100

    test routes

--- src/Components/Connexion.js
+++ src/Components/Connexion.js
@@ -1,11 +1,25 @@
 import React, { Component } from 'react'
 
 export default class Connexion extends Component {
+  state = {
+    pseudov : ''
+  }
+
+  handleChange = event => {
+    const pseudo = event.targetvalue
+    this.setState({ pseudo })
+  }
+  
   render() {
     return (
       <div className='connexionBox'>
         <form className='connexion'>
-            <input placeholder='pseudo' type='text' required/>
+            <input
+            value={this.state.pseudo}
+            onChange='this.handleChange' 
+            placeholder='pseudo' 
+            type='text' 
+            required/>
             <button type='submit'>Go!</button>
         </form>
         
